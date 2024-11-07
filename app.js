7f6cc12be0a3da2e319ffdfe6610f27e134092b6
3AJLFF-3C <00001109488348sp@al.educacao.sp.gov.br>
2024-11-06 21:07:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-.
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    
+}
+let respostaEstaVisivel = false
+container.appendChild(cartao)
+
+
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
