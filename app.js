cbf9d9768f60fc7dd1fe076cc4386634efde2a6d
3AJLFF-3C <00001109488348sp@al.educacao.sp.gov.br>
2024-11-07 17:02:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,16 +12,15 @@ cartao.innerHTML=`
                                 <p>${resposta}</p>
                             </div>
                         </div>
-                        `
+ 
+   ` let respostaEstaVisivel = false 
+  function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+   cartao.addEventListener('click', viraCartao)
+
 container.appendChild(cartao)
     
 }
-let respostaEstaVisivel = false
 
-
-
-function viraCartao() {
-    respostaEstaVisivel = !respostaEstaVisivel
-    cartao.classList.toggle('active', respostaEstaVisivel)
 }
-cartao.addEventListener('click', viraCartao)
